43f3b8d16f36678d917c1517c06be3528de7a9bc
mohammadtalha <mdtalhajamadar@gmail.com>
2025-12-23 12:27:41 +0530

    add program

---
+++ swap.py
@@ -0,0 +1,15 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+
+print("before swapping;")
+print("a=",a)
+print("b='",b)
+
+a,b=b,a
+
+print("after swapping:")
+print("a=",a)
+print("b=",b)
+
+
+      
